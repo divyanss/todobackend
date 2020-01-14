800e46d243e3f6a38376726692afffd78b915faf
divyanss <divyanshsaxena00@gmail.com>
2020-01-14 16:09:27 +0100

    Ready to be released

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 . /appenv/bin/activate
 
-# Install application test requirements
-pip install -r requirements_test.txt
+
+# Download requirements to build cache
+pip download -d /build -r requirements_test.txt --no-input
+
+# Install application test requirements and do not download any dependency instead find in /build cashe
+pip install --no-index -f /build -r requirements_test.txt   
 
 # Run test.sh arguments
 exec $@
